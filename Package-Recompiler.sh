e63e32efa21d77a355ecb22994b91684963679e6
@RubenKelevra <cyrond@gmail.com>
2014-08-31 17:40:17 +0200

    fix wrong if statement

--- Package-Recompiler.sh
+++ Package-Recompiler.sh
@@ -249,7 +249,7 @@ for e in "${deps_worklist[@]}"; do
     if [ -z "$tmp2" ]; then
       echo "ERROR: we seem to have no Internet-connection..."; exit 1
     fi
-    if [ "$tmp1" -ne "$tmp2" ]; then
+    if [ "$tmp1" == "$tmp2" ]; then
       echo "local / remote version missmatch, can't recompile."
       continue
     fi
